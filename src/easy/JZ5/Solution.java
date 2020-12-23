ab3f2aa310592e2b23581ff9776337eb9e39c16b
zhong <hello@zhong.cn>
2020-12-23 23:48:31 +0800

    day1

---
+++ src/easy/JZ5/Solution.java
@@ -0,0 +1,21 @@
+package easy.JZ5;
+
+import java.util.Scanner;
+
+public class Solution {
+    public int Fibonacci(int n) {
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
+        int one=0;
+        int two=1;
+        int sum=0;
+        for(int i=2;i<=n;i++){
+            sum=one+two;
+            one=two;
+            two=sum;
+        }
+        return sum;
+    }
+}
