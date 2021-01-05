9ddf0246ec82cadf66e98c7e7c535fb25445f16e
zhong <hello@zhong.cn>
2021-01-05 13:34:02 +0800

    day10

---
+++ src/hard/JZ29/Solution.java
@@ -0,0 +1,9 @@
+package hard.JZ29;
+
+import java.util.*;
+public class Solution {
+    public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+        ArrayList<Integer> list=new ArrayList<>();
+        return list;
+    }
+}
