ad6392f4e8608e41448ecf6eceea877e9244b8dc
岛田悠米 <115274737@qq.com>
2021-03-30 21:07:42 +0800

    复习JZ medium题解

--- src/medium/JZ36/Solution.java
+++ src/medium/JZ36/Solution.java
@@ -1,16 +1,15 @@
 package medium.JZ36;
 import java.util.*;
 
-class ListNode {
-    int val;
-    ListNode next = null;
-
-    ListNode(int val) {
-        this.val = val;
-    }
-}
-
 public class Solution {
+    public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
     public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
         HashMap<ListNode,Integer> map=new HashMap<>();
         while(pHead1!=null){
